78628dddcb529393244cb48a62566621cf12b9f2
Eric Wu <41607327+Font-Of-Life@users.noreply.github.com>
2021-04-15 14:21:25 -0600

    Added CSS to these files
    added some css, not final up to you guys to change it if ya want

--- Med-System/registerTechnican.php
+++ Med-System/registerTechnican.php
@@ -59,11 +59,42 @@ session_start();
             <title>Technican Registration</title>
             <link rel="stylesheet" href="style.css" type="text/css">
     </head>
-    
+    <style>
+        #buttonStuff{
+            background-color: #0BDA51;
+            color: black;
+            padding: 0.5rem;
+            font-size: 14px;
+            font-weight: bold;
+            border: 2px solid black;
+            border-radius: 1px solid black;
+        }
+
+        #formbox{
+            /*Reference for code used in vertical and horizontal aligment by user Mr Bullets: 
+                -> https://stackoverflow.com/questions/19461521/how-to-center-an-element-horizontally-and-vertically */
+            right: 50%;
+            bottom: 50%;
+            transform: translate(50%,50%);
+            position: absolute;
+            text-align: center;
+            vertical-align: center;
+            margin: 0 auto;
+            background-color: whitesmoke;
+            border-radius: 2px;
+            border: 3px solid black;
+            width: 25%;
+            padding: 5%;
+        }
+
+        body{
+            background-color: darkgrey;
+        }
+    </style>
     <body>
             <div id="formbox">
                     <form method="post">
-                            <div style="font-size: 22px; margin: 14px; color: black; font-weight:bold;">User Registration</div>
+                            <div style="font-size: 22px; margin: 14px; color: black; font-weight:bold;">Technician Registration</div>
                             <p><?php echo $message?></p>
                             <p>
                                 <label>TechID:</label>
@@ -89,8 +120,8 @@ session_start();
                             <input id="buttonStuff" type="submit" value="Register">
                             <br>
                             <br>
-                            <a href="LoginPage.php">Login</a><br><br>
-                            <a href="registerPharmacist.php">Register As Pharmacist</a><br><br>
+                            <a href="LoginPage.php" style="font-size: 17px; color: black;">Login</a><br><br>
+                            <a href="registerPharmacist.php" style="font-size: 17px; color: black;">Register As Pharmacist</a><br><br>
                     </form>
             </div>
     </body>
