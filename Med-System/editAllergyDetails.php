9b25ebdd944abc7c267cc29aa07d5b6edc9d4542
Eric Wu <toomuchepicness@hotmail.com>
2021-04-16 06:49:15 -0600

    added css to index.php and code for editallergy
    editAllergyDetails dont work though...

--- Med-System/editAllergyDetails.php
+++ Med-System/editAllergyDetails.php
@@ -1,51 +1,69 @@
 <?php
-    session_start();
+session_start();
 
     // include the following php files
     include("connections.php");
     include("LoginChecker.php");
-    
-    $userDataSessions = isLoggedIn($conn);
-    //placeholder document for now
     $healthcard = $_SESSION['HC'];
 
     // using SERVER to check if the user has clicked on the post button (if request method = POST)
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
         // collect data from the post variables in the html section below
+        //Ingredient_Name	Drug/Ingredient_Alt	Drug/Ingredient_Usage
         $Ingredient_Name = $_POST['Ingredient_Name'];
-        //$DrugIngredient_Alt = $_POST['Drug/Ingredient_Alt'];
-        //$DrugIngredient_Usage = $_POST['Drug/Ingredient_Usage'];
+        $DrugIngredient_Alt = $_POST['Ingredient_AltName'];
+        $DrugIngredient_Usage = $_POST['Ingredient_Usage'];
 
         $message = "";
 
         // if not empty and it is valid, then do the following
-        if (!empty($Ingredient_Name)) {
+        if (!empty($Ingredient_Name) && !empty($DrugIngredient_Usage) && !empty($DrugIngredient_Alt)) {
+            if(is_numeric($healthcard)){
+                $conditionalQuery = "select * from patient_profile where Gov_HealthCard_Num = '$healthcard'";
+                $CondQueryRes = mysqli_query($conn, $conditionalQuery);
 
-            $conditionalQuery = "select * from can_have where Gov_HealthCard_Num = '$healthcard'";
-            $CondQueryRes = mysqli_query($conn, $conditionalQuery);
+                if(mysqli_num_rows($CondQueryRes) > 0){
+                    $conditionalQuery1 = "select * from drug/ingredient_allergies where Ingredient_Name = '$Ingredient_Name'";
+                    $CondQueryRes1 = mysqli_query($conn, $conditionalQuery1);
 
-            if(mysqli_num_rows($CondQueryRes) > 0){
-                //Ingredient_Name,Gov_HealthCard_Num
-                $sqlQuery = "UPDATE `can_have` SET `Ingredient_Name` = '$Ingredient_Name' WHERE `can_have`.`Gov_HealthCard_Num` = $healthcard";
-                //$sqlQuery = "UPDATE `patient_profile` SET `Weight` = ?, `Height` = ?, `Address` = ?, `Provider_Notes` = ?, `Email` = ?, `COVID_Test_Result` = $covidStat, `Phone` = $phone, WHERE `patient_profile`.`Gov_HealthCard_Num` = $healthcard";
-                mysqli_query($conn, $sqlQuery);
-                $_SESSION['Gov_HealthCard_Num'] = $healthcard;
-                header("Location: viewPatientDetails.php");
-                exit;
+                    if(mysqli_num_rows($CondQueryRes1) > 0){
+                        $checkCanHave = "select * from can_have where Gov_HealthCard_Num = '$healthcard' and Ingredient_Name = '$Ingredient_Name'";
+                        $canHaveStat = mysqli_query($checkCanHave);
+
+                        if(mysqli_num_rows($canHaveStat) < 0){
+                            $condQuery1 = "UPDATE `can_have` SET `Ingredient_Name` = '$Ingredient_Name' WHERE `can_have`.`Gov_HealthCard_Num` = $healthcard";
+                            mysqli_query($conn, $condQuery1);
+                            $_SESSION['Gov_HealthCard_Num'] = $healthcard;
+                            header("Location: viewPatientDetails.php");
+                            exit;
+                        } 
+                        else {
+                            $message = "Patient's allergy already exists in database.";
+                        }
+                    } 
+                    else {
+                        $queryRes = "insert into drug/ingredient_allergies (Ingredient_Name, Drug/Ingredient_Alt, Drug/Ingredient_Usage) values ('$Ingredient_Name','$DrugIngredient_Alt','$DrugIngredient_Usage')";
+                        //insert the following variable queryRes into the user table in the sql Server to update the database in the SQL server
+                        mysqli_query($conn, $queryRes);
+
+                        $queryRes2 = "insert into can_have (Ingredient_Name, Gov_HealthCard_Num) values ('$Ingredient_Name','$healthcard')";
+                        mysqli_query($conn, $queryRes2);
+                        $_SESSION['Gov_HealthCard_Num'] = $healthcard;
+                        header("Location: viewPatientDetails.php");
+                        exit;
+                    }
+                }
+                else{
+                    //echo "Gov. Health card already exists, try again.";
+                    $message = "Gov. Health card does not exists, try again.";
+                } 
             }
-            else{
-                $sqlQuery = "insert into `can_have` (Ingredient_Name, Gov_HealthCard_Num) values ('$Ingredient_Name','$healthcard')";
-                mysqli_query($conn, $sqlQuery);
-                $_SESSION['Gov_HealthCard_Num'] = $healthcard;
-                header("Location: viewPatientDetails.php");
-                exit;
-            }
+            
         } 
         else {
             //echo "The only empty fields allowed are provider notes and email, try again.";
-            $message = "No changes recorded, try again.";
+            $message = "Cannot have a empty field, try again.";
         }
-        mysqli_stmt_close($check);
     }
 ?>
 <!DOCTYPE html>
@@ -137,12 +155,21 @@
                     <div style="font-size: 22px; margin: 14px; color: black; font-weight:bold;">Patient <?php echo $healthcard?> Allergy Edit:</div>
                     <p><?php echo $message?></p>
                     <form method="post">
+                            <p><?php echo $message?></p>
                             <p>
                                 <label>Ingredient Name:</label>
                                 <input type="text" id="textbox" name="Ingredient_Name"/>
                             </p>
+                            <p>
+                                <label>Ingredient Alt Name:</label>
+                                <input type="text" id="textbox" name="Ingredient_AltName"/>
+                            </p>
+                            <p>
+                                <label>Ingredient Usage:</label>
+                                <input type="text" id="textbox" name="Ingredient_Usage"/>
+                            </p>
 
-                            <input id="buttonStuff" type="submit" value="Save Changes"/>
+                            <input type="submit" value="Save" id="buttonStuff"/>
                             <br>
                     </form>
             </div>
